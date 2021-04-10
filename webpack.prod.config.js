37bc69a5ea7b7903a5501e70417258cae97ae6ac
Devvicktor <vkosgei001@gmail.com>
2021-04-10 07:14:10 +0300

    build

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -41,7 +41,7 @@ module.exports = {
  },
  output: {
      // The build folder.
-     path: resolveApp('dist'),
+     path: resolveApp('build'),
      // Generated JS file names (with nested folders).
      // There will be one main bundle, and one file per asynchronous chunk.
      // We don't currently advertise code splitting but Webpack supports it.
